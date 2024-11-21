bc905cb2ec24e82ba8c218fb672671974715c99c
Maria Luz <135741080+M4ri4Edd@users.noreply.github.com>
2024-11-21 14:59:43 -0300

    Update Footer.js

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BsTwitter } from "react-icons/bs";
+import { FaSquareXTwitter } from "react-icons/fa6";
 import { RiInstagramFill } from "react-icons/ri";
 
 const Footer = () => {
@@ -12,8 +12,12 @@ const Footer = () => {
         <div className="footer-section-two">
           <div className="footer-section-columns">
             <div className="footer-icons">
-              <BsTwitter />
-              <RiInstagramFill/>
+              <a href="https://x.com/MaLicht09" target="_blank" rel="noopener noreferrer">
+                <FaSquareXTwitter />              
+              </a>
+              <a href="https://www.instagram.com/mariaeddy63/?next=%2F" target="_blank" rel="noopener noreferrer">
+                <RiInstagramFill/>
+              </a>
             </div>
           </div>
           <div className="footer-section-columns">
